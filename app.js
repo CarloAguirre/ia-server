15f521321b68cc59f4eaa13eff28d428dcff6f4c
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-04-07 17:24:11 -0400

    classes added

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const dotenv = require('dotenv')
-dotenv.config();
+import * as dotenv from 'dotenv'
+dotenv.config()
+import { Server } from "./models/server.js";
 
-const app = express();
-const port = process.env.PORT;
-const apiKey = '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';
-
-app.use(cors());
-app.use(express.json());
-
-app.post('/api/gpt', async (req, res) => {
-    
-    const { input } = req.body;
-  
-    try {
-      const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-        prompt: input,
-        max_tokens: 150,
-        temperature: 0.5,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
-        },
-      });
-  
-      const text = response.data.choices[0].text;
-  
-      res.status(200).send(text);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
-
+const server = new Server();
+server.listen();
--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
-import express  from "express"
-const app = express()
+import express from 'express'
+import axios from 'axios'
+import cors from 'cors'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -7,16 +8,42 @@ dotenv.config()
 class Server {
 
     constructor() {
-        this.app = app;
+        this.app = express();
         this.port = process.env.PORT;
+        this.APIKEY = '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';
 
-        this.routes()
+        this.middlewares();
+        this.routes();
     }
-
+    
     routes() {
-        this.app.get('/', function (req, res) {
-            res.send('Hello World')
-          })
+        this.app.post('/api/gpt', async (req, res) => {
+            const { input } = req.body;
+            
+            try {
+                const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+                    prompt: input,
+                    max_tokens: 150,
+                    temperature: 0.5,
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${this.APIKEY}`,
+                    },
+                });
+                
+                const text = response.data.choices[0].text;
+                
+                res.status(200).send(text);
+            } catch (error) {
+                res.status(500).send(error);
+            }
+        });
+    }
+    
+    middlewares (){
+        this.app.use(express.json());    
+        this.app.use(cors());
     }
 
     listen() {
