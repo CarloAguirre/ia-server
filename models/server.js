e8b8cba636aae48354c559fa0a721a24ab280f9c
Carlo Aguirre <carlo_aguirre@outlook.cl>
2024-05-01 19:32:16 -0400

    some fixes

--- models/server.js
+++ models/server.js
@@ -28,7 +28,7 @@ class Server {
                 const completion = await openai.chat.completions.create({
                     messages: [{
                         role: "system",
-                        content: `puedes darme referencia concretas entre ${tag} con 8 referencias bíblicas VERDADERAS (no inventes nada)?, es decir, contrasta capítulos, libros o referencias y explica brevemente (maximo: 336 caracteres) sus relaciones. Por favor, devuelve esto en la estructura de un json de tipo: [{"concordancia": libro capitulo versiculos, "relacion": aqui va la explicacion de como ambos se relacionan}]. Si no existen referencias o ${tag} es una expresion que NO APARECE EN LA BIBLIA, entonces devuelve lo siguiente: [{}]... recuerda responder en el formato pedido ya que tu respuesta sera manejada de la siguiente manera:
+                        content: `puedes darme referencia concretas entre ${tag} con al menos 8 referencias bíblicas VERDADERAS (no inventes nada)?, es decir, contrasta capítulos, libros o referencias y explica brevemente (maximo: 336 caracteres) sus relaciones. Por favor, devuelve esto en la estructura de un json de tipo: [{"concordancia": libro capitulo versiculos, "relacion": aqui va la explicacion de como ambos se relacionan, "libro": nombre del libro, "capitulo": capitulo que contiene al versiculo}]. Si no existen referencias o ${tag} es una expresion que NO APARECE EN LA BIBLIA, entonces devuelve lo siguiente: [{}]... recuerda responder en el formato pedido ya que tu respuesta sera manejada de la siguiente manera:
                         const responseText = completion.choices[0].message.content;
                         const concordancias = JSON.parse(responseText);
                         res.status(200).json( concordancias );....por lo que tu respuesta siempre deben ser objetos dentro de un array`
@@ -38,6 +38,7 @@ class Server {
                 
                 const responseText = completion.choices[0].message.content;
                 const concordancias = JSON.parse(responseText); // Parsea el texto JSON a un objeto
+            
                 res.status(200).json( concordancias );
             }
             
